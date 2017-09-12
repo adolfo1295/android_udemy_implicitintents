c602e2fbca842967cfb7ebf0c9700950d1ea718a
Adolfo Chavez <adolfo.dev1295@gmail.com>
2017-09-12 15:24:05 -0500

    ultimo commit intent implicitos
    se termino el codigo de intents implicitos

--- Intentsimplicitos/app/src/main/java/implicitos/intents/curso/udemy/adolfo/com/intentsimplicitos/MainActivity.java
+++ Intentsimplicitos/app/src/main/java/implicitos/intents/curso/udemy/adolfo/com/intentsimplicitos/MainActivity.java
@@ -1,12 +1,16 @@
 package implicitos.intents.curso.udemy.adolfo.com.intentsimplicitos;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 
 /**
  * intent implicitos
+ * son los que pasan datos a otras aplicaciones
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        implicitIntent2();
     }
+
+    public void implicitIntent(View view) {
+        Intent implicitIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
+        startActivity(implicitIntent);
+    }
+
+    private void implicitIntent2(){
+        Intent implicitIntent = new Intent(Intent.ACTION_SEND);
+        implicitIntent.setType("text/plain");
+        implicitIntent.putExtra(Intent.EXTRA_TEXT,"Que onda perros");
+        startActivity(implicitIntent);
+    }
+
 }
